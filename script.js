f09e103bd221e27c33eccc95442dce6a1b5c5109
= <anumalla.sandeep1999@gmail.com>
2021-08-29 18:09:42 +0530

    added functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+const videoElement = document.getElementById("video_container");
+const button = document.getElementById("button");
+
+async function request() {
+  try {
+    let captureMedia = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.hidden = false;
+    videoElement.srcObject = captureMedia;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+    // return captureMedia
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+button.addEventListener("click", request());
